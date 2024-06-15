3a560188d30e6dbc297fb0e18802038922118052
talatkuyuk <talatkuyuk@gmail.com>
2024-06-15 23:22:57 +0300

    added project structure

---
+++ vite.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+
+/// <reference types="vitest" />
+export default defineConfig({
+  test: {
+    include: ["tests/**/*.spec.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: [
+        ["lcov", { projectRoot: "./src" }],
+        ["json", { file: "coverage.json" }],
+        "text",
+      ],
+      exclude: ["archive", "tests", "**/*.d.ts"],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
+  },
+});
